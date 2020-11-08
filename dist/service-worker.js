a3e0b31576089bfd8fd77c258fa9a5b1ff67cb38
ajwehunt <adamjwehunt@gmail.com>
2020-11-08 13:11:07 -0700

    prod build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b569520cb55c1fbfbb5ab1a63f7fd8d.js"
+  "/precache-manifest.06b85387c4b2e5fc1e462b8112292434.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "st-installations"});
